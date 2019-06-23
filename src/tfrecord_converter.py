1fd35049dd5460f00653b625885bf42a4b2d5198
Giovanni Bruner <giovannibruner@Giovannis-MacBook-Air.local>
2019-06-23 10:52:09 +0200

    adding sys path to import object detection

--- src/tfrecord_converter.py
+++ src/tfrecord_converter.py
@@ -8,7 +8,7 @@ import pandas as pd
 import tensorflow as tf
 import sys
 
-# sys.path.append("../../models/research")
+sys.path.append("../../models/research")
 
 from PIL import Image
 from object_detection.utils import dataset_util
